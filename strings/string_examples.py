9d9c59e19a6835d292852f7d3a31b1789087b400
theAICrafter101 <nainhaider02@gmail.com>
2026-01-08 00:20:23 +0200

    Learn about strings & their manipulation in Python

---
+++ strings/string_examples.py
@@ -0,0 +1,23 @@
+my_str = 'Hello world'
+
+print('Hello' in my_str)  # True
+print('hey' in my_str)    # False
+print('hi' in my_str)    # False
+print('e' in my_str)  # True
+print('f' in my_str)  # False
+
+my_str = 'Hello world'
+print(len(my_str))  # 11
+
+my_str = "Hello world"
+
+print(my_str[0])  # H
+print(my_str[6])  # w
+
+my_str = 'Hello world'
+print(my_str[-1])  # d
+print(my_str[-2]) # l
+
+greeting = 'hi'
+greeting = 'hello'
+print(greeting) # hello
