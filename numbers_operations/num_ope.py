897d1bc97bd3774973c4cd6750ebc55692fdd2e8
theAICrafter101 <nainhaider02@gmail.com>
2026-01-09 18:36:34 +0200

    Learnt basic operations & augmented assignments.

---
+++ numbers_operations/num_ope.py
@@ -0,0 +1,139 @@
+# Addition of two integers
+my_int_1 = 5
+my_int_2 = 10
+my_sum = my_int_1 + my_int_2
+print(my_sum) # This will output 15
+
+# Subtraction of two integers
+my_int_1 = 15
+my_int_2 = 5
+my_difference = my_int_1 - my_int_2
+print(my_difference) # This will output 10
+
+# Multiplication of two integers
+my_int_1 = 4
+my_int_2 = 3
+my_product = my_int_1 * my_int_2
+print(my_product) # This will output 12
+
+# Division of two integers
+my_int_1 = 20
+my_int_2 = 4
+my_quotient = my_int_1 / my_int_2
+print(my_quotient) # This will output 5.0
+
+# Addition of two floats
+my_float_1 = 5.5
+my_float_2 = 10.2
+my_float_sum = my_float_1 + my_float_2
+print(my_float_sum) # This will output 15.7
+
+# Subtraction of two floats
+my_float_1 = 12.5
+my_float_2 = 3.2
+my_float_difference = my_float_1 - my_float_2
+print(my_float_difference) # This will output 9.3
+
+# Multiplication of two floats
+my_float_1 = 2.5
+my_float_2 = 4.0
+my_float_product = my_float_1 * my_float_2
+print(my_float_product) # This will output 10.0
+
+# Division of two floats
+my_float_1 = 9.0
+my_float_2 = 3.0
+my_float_quotient = my_float_1 / my_float_2
+print(my_float_quotient) # This will output 3.0
+
+# Basic arithmetic operations uisng integers and floats in Python
+my_int_1 = 5
+my_float_1 = 2.5
+my_mixed_sum = my_int_1 + my_float_1
+my_mixed_difference = my_int_1 - my_float_1
+my_mixed_product = my_int_1 * my_float_1
+my_mixed_quotient = my_int_1 / my_float_1
+print(my_mixed_sum)          # This will output 7.5
+print(my_mixed_difference)   # This will output 2.5
+print(my_mixed_product)      # This will output 12.5
+print(my_mixed_quotient)     # This will output 2.0
+
+# Modulus
+my_int_1 = 10
+my_int_2 = 3
+my_modulus = my_int_1 % my_int_2
+my_float_1 = 10.5
+my_float_2 = 3.2
+my_float_modulus = my_float_1 % my_float_2
+print(my_modulus)         # This will output 1
+print(my_float_modulus)   # This will output 0.899999999999998
+
+# Floor Division
+my_int_1 = 10
+my_int_2 = 3
+my_floor_division = my_int_1 // my_int_2
+my_float_1 = 10.5
+my_float_2 = 3.2
+my_float_floor_division = my_float_1 // my_float_2
+print(my_floor_division)         # This will output 3
+print(my_float_floor_division)   # This will output 3.0
+
+# Exponentiation
+my_int_1 = 2
+my_int_2 = 3
+my_exponentiation = my_int_1 ** my_int_2
+my_float_1 = 2.0
+my_float_2 = 3.0
+my_float_exponentiation = my_float_1 ** my_float_2
+print(my_exponentiation)         # This will output 8
+print(my_float_exponentiation)   # This will output 8.0
+
+# Float Conversion
+my_int = 56
+my_converted_float = float(my_int)
+print(my_converted_float)  # This will output 56.0
+
+# Integer Conversion
+my_float = 78.9
+my_converted_int = int(my_float)
+print(my_converted_int)    # This will output 78
+
+# String to Integer & Float Conversion
+my_str_int = "123"
+my_str_float = "45.67"
+my_converted_int = int(my_str_int)
+my_converted_float = float(my_str_float)
+print(my_converted_int)      # This will output 123
+print(my_converted_float)    # This will output 45.67
+
+# Rounding Numbers
+my_float_1 = 56.789
+my_float_2 = 56.123
+rounded_float_1 = round(my_float_1)
+rounded_float_2 = round(my_float_2, 2)
+print(rounded_float_1)  # This will output 57
+print(rounded_float_2)  # This will output 56.12
+
+# Absolute Value
+my_int = -25
+my_float = -45.67
+abs_int = abs(my_int)
+abs_float = abs(my_float)
+print(abs_int)    # This will output 25
+print(abs_float)  # This will output 45.67
+
+# Power Function
+base_int = 3
+exponent_int = 4
+power_int = pow(base_int, exponent_int)
+print(power_int)  # This will output 81
+
+# Augmented Assignment Operators
+my_int = 10
+my_int += 5      # Equivalent to my_int = my_int + 5
+print(my_int)   # This will output 15
+
+# String augmented assignment
+my_str = "Hello"
+my_str += " World!"  # Equivalent to my_str = my_str + " World!"
+print(my_str)        # This will output "Hello World!"
